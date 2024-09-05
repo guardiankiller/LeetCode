d03723810186f00cbda2eb9e37ddf8feec50dd93
guardiankiller <com_1998@abv.bg>
2024-09-06 00:20:51 +0300

    removed main method from RemoveDuplicatesFromSortedArray

--- src/RemoveDuplicatesFromSortedArray.java
+++ src/RemoveDuplicatesFromSortedArray.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class RemoveDuplicatesFromSortedArray {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static int removeDuplicates(int[] nums) {
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
     }
 }
