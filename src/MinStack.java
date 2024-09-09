62c49223d12dba84eff2e73d65166bafb24d07e4
guardiankiller <com_1998@abv.bg>
2024-09-09 13:24:10 +0300

    Added MinStack

---
+++ src/MinStack.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+public class MinStack {
+    private final Stack<Integer> stack;
+    private final Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (stack.size() == 1) {
+            minStack.push(val);
+        } else {
+            minStack.push(Math.min(minStack.peek(), val));
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
